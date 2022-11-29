d8ae5086557b9941d5843af9c507d1ae223a7a47
Hadizadefemi <adefemihadizi2020@gmail.com>
2022-11-29 20:24:28 +0000

    Task 8 done

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 65 and ord(i) <= 90:
+            print("{}".format(i), end="")
+        else:
+            print("{}".format(chr(ord(i - 32))), end="")
