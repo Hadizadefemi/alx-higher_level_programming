9808411a0a294ab310b591a2c0e1892d191db608
Hadizadefemi <adefemihadizi2020@gmail.com>
2022-12-03 19:35:42 +0000

    Fixed

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,7 +5,8 @@ def replace_in_list(my_list, idx, element):
     elif idx >= len(my_list):
         return my_list
     else:
-        for i in my_list:
+        for i in range(len(my_list)):
             if i == idx:
                 my_list[idx] = element
+                break
     return my_list
