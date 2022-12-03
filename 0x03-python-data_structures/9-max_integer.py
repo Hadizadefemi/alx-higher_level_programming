2dbb4d52c75f87826732303296cb4c947e94e017
Hadizadefemi <adefemihadizi2020@gmail.com>
2022-12-03 22:03:35 +0000

    Task 9 completed

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -8,5 +8,5 @@ def max_integer(my_list=[]):
         max_num = my_list[0]
         for i in my_list:
             if i > max_num:
-                 max_num = i
+                max_num = i
     return max_num
