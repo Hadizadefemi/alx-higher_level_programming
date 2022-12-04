dee1b9b9ac505b1b253dce1ef78e6be66a245869
Hadizadefemi <adefemihadizi2020@gmail.com>
2022-12-04 21:44:28 +0000

    Do you see me now checker?

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         sentence[0] == None
     else:
-        my_tuples = (len(sentence), sentence[0])
-        return my_tuples
+        my_tuple = (len(sentence), sentence[0])
+        return my_tuple
