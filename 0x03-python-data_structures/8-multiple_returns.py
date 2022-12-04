819ff2c0323d0392c9f988d74f420a01c550275c
Hadizadefemi <adefemihadizi2020@gmail.com>
2022-12-04 21:46:47 +0000

    Do you see me now checker?

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,8 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
         sentence[0] == None
+    if sentence is None:
+        return
     else:
         my_tuple = (len(sentence), sentence[0])
         return my_tuple
