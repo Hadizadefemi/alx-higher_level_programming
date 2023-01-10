be5c25d95ca1f1c5bbd0eaa31c5c535f33da97b3
Hadizadefemi <adefemihadizi2020@gmail.com>
2023-01-10 23:20:12 +0000

    Task 2 done

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,19 @@
+ #!/usr/bin/python3
+""" Module 2-append_write
+function that appends a string at the end of a text file (UTF8)
+and returns the number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """ appends a string to a text file
+    Args:
+        filename: file to append text to
+        text: string to append to filename
+    Returns:
+        number of characters added
+    """
+
+    with open(filename, mode="a", encoding="utf-8") as f:
+        numOf_char = f.write(text)
+    return numOf_char
