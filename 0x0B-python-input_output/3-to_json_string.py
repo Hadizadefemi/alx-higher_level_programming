4df5d09cf68806171d029a482dc32b22d6f4e838
Hadizadefemi <adefemihadizi2020@gmail.com>
2023-01-10 23:52:56 +0000

    Task 3 done

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module 3-to_json_string.
+function that returns the JSON 
+representation of an object (string)
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ returns JSON representation of an object
+    Args:
+        my_obj: object to represent in JSON
+    Returns:
+        the object in JSON
+    """
+
+    return json.dumps(my_obj)
