7ecb15d573817dda3b9e75702b95522d8eea23c1
Hadizadefemi <adefemihadizi2020@gmail.com>
2023-01-10 19:56:32 +0000

    Task corrected

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Module 0-read_file.
-
 defines a functuon that reads a text file
 and prints in stdout
 """
@@ -8,7 +7,6 @@ and prints in stdout
 
 def read_file(filename=""):
     """prints content of the text file to stdout
-    
     Args:
         filename: the file to read
     """
