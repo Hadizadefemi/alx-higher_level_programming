e2c247a6bd287244571788c5b643651eb01634e6
Hadizadefemi <adefemihadizi2020@gmail.com>
2023-01-10 14:52:29 +0000

    added README and Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module 0-read_file.
+
+defines a functuon that reads a text file
+and prints in stdout
+"""
+
+
+def read_file(filename=""):
+    """prints content of the text file to stdout
+    
+    Args:
+        filename: the file to read
+    """
+
+    with open(filename, encoding="utf-8") as f:
+        data = f.read()
+    print(data)
