f4c8353cc80b1a6417520b6aed934da6d2f5a8bd
Hadiz Adefemi <111155656+Hadizadefemi@users.noreply.github.com>
2023-01-15 20:59:53 +0100

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,16 +13,20 @@ def pascal_triangle(n):
     """
 
     matrix = []
-    if n <= 0:
-        return matrix
+    prev = []
 
     for i in range(n):
-        inner = [str(11**i)]
-        res = []
-        for i in inner:
-            for j in i:
-                res.append(int(j))
-
-        matrix.append(res)
+        res_list = []
+        p1 = -1
+        p2 = 0
+        for j in range(len(prev) + 1):
+            if p1 == -1 or p2 == len(prev):
+                res_list += [1]
+            else:
+                res_list += [prev[p1] + prev[p2]]
+            p1 += 1
+            p2 += 1
+        matrix.append(res_list)
+        prev = res_list[:]
 
     return matrix
