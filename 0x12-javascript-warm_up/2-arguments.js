76d3c041a8e4ad46f6af73b238fd906510f9f1c5
Hadizadefemi <adefemihadizi2020@gmail.com>
2023-03-13 23:17:52 +0000

    Task 2 corrected

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ const process = require('process');
 const argv = process.argv;
 
 if (argv.length === 2) {
-  console.log('No arguments');
+  console.log('No argument');
 } else if (argv.length === 3) {
   console.log('Argument found');
 } else {
