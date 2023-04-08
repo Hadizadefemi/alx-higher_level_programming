237c518d8a56434e3493884890a8c0ca6874f849
Hadiz Adefemi <111155656+Hadizadefemi@users.noreply.github.com>
2023-04-08 20:58:39 +0100

    Update 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,11 +6,23 @@ after each line containing a specific string.
 
 
 def append_after(filename="", search_string="", new_string=""):
-    with open(filename, mode="r+", encoding="utf-8") as f:
-        lines = f.readlines()
-        f.seek(0)
-        for line in lines:
-            f.write(line)
-            if search_string in line:
-                f.write(new_string)
-        f.truncate()
+    """ inserts new_string, after each line containing a search_string
+    Args:
+        filename: name of file to modify with new_string
+        search_string: string to search for
+        new_string: string to insert to fielname
+    """
+
+    with open(filename, mode='r', encoding="utf-8") as f:
+        data = list(f)
+
+    with open(filename, mode="w", encoding="utf-8") as f:
+        if len(data) > 1:
+            for i in range(len(data)):
+                if search_string in data[i]:
+                    data.insert(i + 1, new_string)
+
+        if search_string in data:
+            data.insert(i + 1, new_string)
+
+        f.writelines(data)
